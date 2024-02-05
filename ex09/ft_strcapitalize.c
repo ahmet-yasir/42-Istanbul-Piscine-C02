93ed228edd944d6271f7864ef05c10dc29646815
Ahmet Yasir <ahmetyasirkulaksiz@gmail.com>
2024-02-05 14:31:40 -0500

    ex09

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/05 13:59:01 by akulaksi          #+#    #+#             */
-/*   Updated: 2024/02/05 14:19:59 by akulaksi         ###   ########.fr       */
+/*   Created: 2024/02/05 14:11:06 by akulaksi          #+#    #+#             */
+/*   Updated: 2024/02/05 14:30:23 by akulaksi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,34 @@ char    *ft_strlowcase(char *str)
     return (str);
 }
 
-/*int main (void)
+char    *ft_strcapitalize(char *str)
 {
-    char str[] = "";
+    int i;
+
+    i = 1;
+    ft_strlowcase(str);
+    while (str[i] != '\0')
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') && (!((str[i-1] >= 'a' && str[i-1] <= 'z') || (str[i-1] >= 'A' && str[i-1] <= 'Z'))))
+            str[i] -= 32;
+        i++;
+    }
+    if ((str[0] >= 'a' && str[i] <= 'z'))
+        str[0] -=32;
+    return (str);
+}
+
+int main (void)
+{
+    char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
     int i;
 
     i = 0;
-    ft_strlowcase(str);
+    ft_strcapitalize(str);
     while (str[i] != '\0')
     {
         write(1,&str[i], 1);
         i++;
     }
     return(0);
-}*/
+}
